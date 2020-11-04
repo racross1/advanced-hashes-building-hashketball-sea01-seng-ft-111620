40bd2860fc5cf4e5c134a193e80c71d3dbb9f993
Rachel Cross <racross1@gmail.com>
2020-11-04 20:36:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -25,6 +25,36 @@ def game_hash
           blocks: 1, 
           slam_dunks: 1
       },
+      "Alan Anderson" => {
+          number: 0, 
+          shoe: 16, 
+          points: 22, 
+          rebounds: 12, 
+          assists: 12, 
+          steals: 3, 
+          blocks: 1, 
+          slam_dunks: 1
+      },
+      "Alan Anderson" => {
+          number: 0, 
+          shoe: 16, 
+          points: 22, 
+          rebounds: 12, 
+          assists: 12, 
+          steals: 3, 
+          blocks: 1, 
+          slam_dunks: 1
+      },
+      "Alan Anderson" => {
+          number: 0, 
+          shoe: 16, 
+          points: 22, 
+          rebounds: 12, 
+          assists: 12, 
+          steals: 3, 
+          blocks: 1, 
+          slam_dunks: 1
+      },
     },
     away: {
       team_name: "",
