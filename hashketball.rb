2c5b47a691c70afeca109dfa3923128ffd0d4b7c
Rachel Cross <racross1@gmail.com>
2020-11-04 20:31:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,9 @@ def game_hash
     home: {
       team_name: "Brooklyn Nets",
       colors: ["Black", "White"], 
-      players: [{}]
+      players: {
+        
+      }
     },
     away: {
       team_name: "",
