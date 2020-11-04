392d7fd9adf1fa7fabe315ed32eade3af1428acb
Rachel Cross <racross1@gmail.com>
2020-11-04 21:24:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -95,31 +95,31 @@ def game_hash
           points: 24, 
           rebounds: 12, 
           assists: 12, 
-          steals: 3, 
-          blocks: 1, 
-          slam_dunks: 15 
+          steals: 4, 
+          blocks: 5, 
+          slam_dunks: 5 
         },
         {
           player_name: "Ben Gordon", 
-          number: 1, 
-          shoe: 19, 
-          points: 26, 
-          rebounds: 11, 
-          assists: 6, 
-          steals: 3, 
-          blocks: 8, 
-          slam_dunks: 5
+          number: 8, 
+          shoe: 15, 
+          points: 33, 
+          rebounds: 3, 
+          assists: 2, 
+          steals: 1, 
+          blocks: 1, 
+          slam_dunks: 0
         },
         {
           player_name: "Kemba Walker",
-          number: 31, 
+          number: 33, 
           shoe: 15, 
-          points: 19, 
-          rebounds: 2, 
-          assists: 2, 
-          steals: 4, 
-          blocks: 11, 
-          slam_dunks: 1
+          points: 6, 
+          rebounds: 12, 
+          assists: 12, 
+          steals: 7, 
+          blocks: 5, 
+          slam_dunks: 12
         }
       ]
     }
