d61f09408fff0dbdba7ba93bca1fdb8e91343342
Rachel Cross <racross1@gmail.com>
2020-11-04 20:22:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,12 +1,14 @@
 # Write your code here!
 def game_hash 
   {home: {
-    team_name: 
-    colors: 
-    players:
+    team_name: ""
+    colors: []
+    players: [{}]
     
   }, 
   away: {
-    team
+    team_name: 
+    colors: 
+    players:
   }
   }
